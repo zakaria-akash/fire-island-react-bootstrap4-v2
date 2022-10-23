ec5bb3e659598b89490be58233da74e4afd010e0
zakaria-akash <zakaria.93@yahoo.com>
2022-10-24 02:30:25 +0600

    implementing error handling for Fetch APIs in the Action Creator component by throwing error in a separate response statement

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -30,8 +30,26 @@ export const fetchDishes = () => (dispatch) => {
   dispatch(dishesLoading(true));
 
   return fetch(baseUrl + "dishes")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          let error = new Error(
+            "Error: " + response.status + " : " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        let errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
     .then((response) => response.json())
-    .then((dishes) => dispatch(addDishes(dishes)));
+    .then((dishes) => dispatch(addDishes(dishes)))
+    .catch((error) => dispatch(dishesFailed(error.message)));
 };
 
 export const commentsFailed = (errmess) => ({
@@ -46,8 +64,26 @@ export const addComments = (comments) => ({
 
 export const fetchComments = () => (dispatch) => {
   return fetch(baseUrl + "comments")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          let error = new Error(
+            "Error: " + response.status + " : " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        let errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
     .then((response) => response.json())
-    .then((comments) => dispatch(addComments(comments)));
+    .then((comments) => dispatch(addComments(comments)))
+    .catch((error) => dispatch(commentsFailed(error.message)));
 };
 
 //
@@ -70,6 +106,24 @@ export const fetchPromos = () => (dispatch) => {
   dispatch(promosLoading(true));
 
   return fetch(baseUrl + "promotions")
+    .then(
+      (response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          let error = new Error(
+            "Error: " + response.status + " : " + response.statusText
+          );
+          error.response = response;
+          throw error;
+        }
+      },
+      (error) => {
+        let errMess = new Error(error.message);
+        throw errMess;
+      }
+    )
     .then((response) => response.json())
-    .then((promotions) => dispatch(addPromos(promotions)));
+    .then((promotions) => dispatch(addPromos(promotions)))
+    .catch((error) => dispatch(promosFailed(error.message)));
 };
